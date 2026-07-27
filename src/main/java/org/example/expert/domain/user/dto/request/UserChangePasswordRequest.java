76c93ca1cc451cb7eda1ecc0aad26e12920c4ef0
yeang976-art <yeang976@gmail.com>
2026-07-27 12:28:51 +0900

    레벨 2-3: 검증 어노테이션 조정과 정규식 수정

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -13,18 +13,16 @@ import lombok.NoArgsConstructor;
 public class UserChangePasswordRequest {
 
     @NotBlank
-    @Min(value = 8, message = "비밀번호는 8자 이상이어야 합니다")
     @Pattern(
-            regexp = "^(?=.*[A-Z])(?=.*\\d)$",
-            message = "비밀번호는 숫자와 대문자를 포함해야 합니다."
+            regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$",
+            message = "비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."
     )
     private String oldPassword;
 
     @NotBlank
-    @Min(value = 8, message = "새 비밀번호는 8자 이상이어야 합니다")
     @Pattern(
-            regexp = "^(?=.*[A-Z])(?=.*\\d)$",
-            message = "새 비밀번호는 숫자와 대문자를 포함해야 합니다."
+            regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$",
+            message = "새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다."
     )
     private String newPassword;
 }
